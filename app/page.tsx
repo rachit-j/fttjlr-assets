a07a7b1ac616639c91214352eeced8577a38b9d9
rachit-j <rjaiswal.sd.77@gmail.com>
2024-01-19 09:52:39 -0800

    rename lmao

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>Welcome to Rackets Assets. you should not be here.</p>
+      <p>Welcome to FTTJLR Assets. you should not be here.</p>
     </main>
   );
 }
